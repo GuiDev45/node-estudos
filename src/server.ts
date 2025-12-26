b6eef85a6c56c89d3f9ce33a5bef9f27af72d675
GuiDev45 <bortolettodev@gmail.com>
2025-12-26 17:52:07 -0300

    Aula Response

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import { createServer } from "node:http";
 
-// Como temos um await, a função precisa ser assincrona / async
 const server = createServer(async (req, res) => {
-  res.setHeader("Content-Type", "text/plain; charset=utf-8");
-
   if (!req.url) {
     res.statusCode = 400;
     res.end("URL inválida");
@@ -12,25 +9,38 @@ const server = createServer(async (req, res) => {
 
   const url = new URL(req.url, "http://localhost:3000");
 
-  // Array de Buffers
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
   const chunks = [];
-  // Loop simples para pegar cada parte desses Buffers
   for await (const chunk of req) {
     chunks.push(chunk);
   }
-  // O body chega como buffers, que são bytes. Primeiro convertemos para string e depois fazemos o parse para JSON.
-  const body = Buffer.concat(chunks).toString("utf-8");
-  console.log(JSON.parse(body));
 
   const cor = url.searchParams.get("cor");
   const tamanho = url.searchParams.get("tamanho");
 
   if (req.method === "GET" && url.pathname === "/") {
     res.statusCode = 200;
-    res.end("Home.");
+    // O Content-Type informa ao navegador qual é o tipo do conteúdo da resposta
+    // e como ele deve interpretar esse conteúdo (HTML, JSON, texto etc).
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    // .end é sempre o que vai ser enviado no fim, no caso esse html.
+    // Importante, é apenas 1 end, ele não vai conseguir escrever o corpo novamente.
+    res.end(`
+      <html>
+        <head>
+          <body>
+            <h1>Olá mundo</h1>
+          </body>
+        </head>
+      </html>
+      `);
   } else if (req.method === "POST" && url.pathname === "/produtos") {
     res.statusCode = 201;
-    res.end(`Produtos: ${cor}, ${tamanho}`);
+    // setHeader porém com o conteúdo para um JSON.
+    res.setHeader("Content-Type", "application/json");
+    // O que vai ser enviado é um JSON com as propriedades de cor e tamanho.
+    res.end(JSON.stringify({ cor, tamanho }));
   } else {
     res.statusCode = 404;
     res.end("Não encontrada.");
